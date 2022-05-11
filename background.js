7a8b25777d62ea35392b9eff97f6beb6429872ef
Algaslass <100960548+Algaslass@users.noreply.github.com>
2022-05-12 00:50:08 +0100

    Add files via upload

---
+++ background.js
@@ -0,0 +1,70 @@
+
+ const inputBtn = document.getElementById("input-btn")
+ let myLeads = []
+ let oldLeads = []
+
+ const inputEl = document.getElementById("input-el")
+
+ const ulEl = document.getElementById("ul-el")
+
+ const deleteBtn = document.getElementById("delete-btn")
+
+ const tabBtn = document.getElementById("tab-btn")
+
+ console.log(ulEl)
+
+
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
+
+
+if (leadsFromLocalStorage) { 
+  myLeads = leadsFromLocalStorage
+  render(myLeads)
+}
+
+tabBtn.addEventListener("click", function() {
+ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+   myLeads.push(tabs[0].url)
+  localStorage.setItem("myLeads", JSON.stringify(myLeads)) // save the tab url
+  render(myLeads)
+
+ })
+
+})
+
+function render(leads) {
+ let listItems = ""
+ for (let i = 0; i < leads.length; i++) {
+
+ listItems += `
+          <li>
+              <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
+              </a>
+          <li>
+      `
+      // another way by the template strings
+ }
+ ulEl.innerHTML = listItems
+}
+
+
+
+
+deleteBtn.addEventListener("dblclick", function() { 
+  console.log("double cliked!")
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+  myLeads.push(inputEl.value)  
+  inputEl.value = "" 
+
+  localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
+
+  render(myLeads)
+  console.log(localStorage.getItem("myLeads"))
+})
